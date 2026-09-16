b56e1c676da9ff8c10b6513d17529d3ddf19e024
Vibha Vishwakarma <vvibha71@gmail.com>
2026-09-16 16:29:10 +0530

    Debug production CORS origin

--- server/src/app.js
+++ server/src/app.js
@@ -27,9 +27,12 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
+      console.log("REQUEST ORIGIN:", origin);
+
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
+        console.log("BLOCKED ORIGIN:", origin);
         callback(new Error("Not allowed by CORS"));
       }
     },
